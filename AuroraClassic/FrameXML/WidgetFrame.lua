99069544fab3f3fbde284167cc43d25241db92cc
siweia <siwei1990@outlook.com>
2020-05-10 17:19:59 +0800

    More widget frames

--- AuroraClassic/FrameXML/WidgetFrame.lua
+++ AuroraClassic/FrameXML/WidgetFrame.lua
@@ -2,50 +2,90 @@ local F, C = unpack(select(2, ...))
 
 tinsert(C.themes["AuroraClassic"], function()
 	-- Credits ShestakUI
-    hooksecurefunc(_G.UIWidgetTemplateCaptureBarMixin, "Setup", function(widgetInfo)
-        widgetInfo.LeftLine:SetAlpha(0)
-        widgetInfo.RightLine:SetAlpha(0)
-        widgetInfo.BarBackground:SetAlpha(0)
-        widgetInfo.Glow1:SetAlpha(0)
-        widgetInfo.Glow2:SetAlpha(0)
-        widgetInfo.Glow3:SetAlpha(0)
+	local atlasColors = {
+		["UI-Frame-Bar-Fill-Blue"] = {.2, .6, 1},
+		["UI-Frame-Bar-Fill-Red"] = {.9, .2, .2},
+		["UI-Frame-Bar-Fill-Yellow"] = {1, .6, 0},
+		["objectivewidget-bar-fill-left"] = {.2, .6, 1},
+		["objectivewidget-bar-fill-right"] = {.9, .2, .2}
+	}
 
-        widgetInfo.LeftBar:SetTexture(C.media.backdrop)
-        widgetInfo.NeutralBar:SetTexture(C.media.backdrop)
-        widgetInfo.RightBar:SetTexture(C.media.backdrop)
+	local function updateBarTexture(self, atlas)
+		if atlasColors[atlas] then
+			self:SetStatusBarTexture(C.media.backdrop)
+			self:SetStatusBarColor(unpack(atlasColors[atlas]))
+		end
+	end
 
-        widgetInfo.LeftBar:SetVertexColor(.2, .6, 1)
-        widgetInfo.NeutralBar:SetVertexColor(.8, .8, .8)
-        widgetInfo.RightBar:SetVertexColor(.9, .2, .2)
+	local doubleBarType = _G.Enum.UIWidgetVisualizationType.DoubleStatusBar
+	local frame = CreateFrame("Frame")
+	frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+	frame:RegisterEvent("UPDATE_ALL_UI_WIDGETS")
+	frame:SetScript("OnEvent", function()
+		for _, widgetFrame in pairs(_G.UIWidgetTopCenterContainerFrame.widgetFrames) do
+			if widgetFrame.widgetType == doubleBarType then
+				for _, bar in pairs({widgetFrame.LeftBar, widgetFrame.RightBar}) do
+					if not bar.styled then
+						bar.BG:SetAlpha(0)
+						bar.BorderLeft:SetAlpha(0)
+						bar.BorderRight:SetAlpha(0)
+						bar.BorderCenter:SetAlpha(0)
+						bar.Spark:SetAlpha(0)
+						bar.SparkGlow:SetAlpha(0)
+						bar.BorderGlow:SetAlpha(0)
+						F.SetBD(bar)
+						hooksecurefunc(bar, "SetStatusBarAtlas", updateBarTexture)
 
-        if not widgetInfo.bg then
-            widgetInfo.bg = F.SetBD(widgetInfo)
-            widgetInfo.bg:Point("TOPLEFT", widgetInfo.LeftBar, -2, 2)
-            widgetInfo.bg:Point("BOTTOMRIGHT", widgetInfo.RightBar, 2, -2)
-        end
-    end)
+						bar.styled = true
+					end
+				end
+			end
+		end
+	end)
 
-    local frame = CreateFrame("Frame")
-    frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-    frame:RegisterEvent("UPDATE_ALL_UI_WIDGETS")
-    frame:SetScript("OnEvent", function()
-        for _, widgetFrame in pairs(_G.UIWidgetTopCenterContainerFrame.widgetFrames) do
-            if widgetFrame.widgetType == _G.Enum.UIWidgetVisualizationType.DoubleStatusBar then
-                for _, bar in pairs({widgetFrame.LeftBar, widgetFrame.RightBar}) do
-                    if not bar.styled then
-                        bar.BG:SetAlpha(0)
-                        bar.BorderLeft:SetAlpha(0)
-                        bar.BorderRight:SetAlpha(0)
-                        bar.BorderCenter:SetAlpha(0)
-                        bar.Spark:SetAlpha(0)
-                        bar.SparkGlow:SetAlpha(0)
-                        bar.BorderGlow:SetAlpha(0)
-                        F.SetBD(bar)
+	hooksecurefunc(_G.UIWidgetTemplateCaptureBarMixin, "Setup", function(widgetInfo)
+		widgetInfo.LeftLine:SetAlpha(0)
+		widgetInfo.RightLine:SetAlpha(0)
+		widgetInfo.BarBackground:SetAlpha(0)
+		widgetInfo.Glow1:SetAlpha(0)
+		widgetInfo.Glow2:SetAlpha(0)
+		widgetInfo.Glow3:SetAlpha(0)
 
-                        bar.styled = true
-                    end
-                end
-            end
-        end
-    end)
+		widgetInfo.LeftBar:SetTexture(C.media.backdrop)
+		widgetInfo.NeutralBar:SetTexture(C.media.backdrop)
+		widgetInfo.RightBar:SetTexture(C.media.backdrop)
+
+		widgetInfo.LeftBar:SetVertexColor(.2, .6, 1)
+		widgetInfo.NeutralBar:SetVertexColor(.8, .8, .8)
+		widgetInfo.RightBar:SetVertexColor(.9, .2, .2)
+
+		if not widgetInfo.bg then
+			widgetInfo.bg = F.SetBD(widgetInfo)
+			widgetInfo.bg:Point("TOPLEFT", widgetInfo.LeftBar, -2, 2)
+			widgetInfo.bg:Point("BOTTOMRIGHT", widgetInfo.RightBar, 2, -2)
+		end
+	end)
+
+	hooksecurefunc(_G.UIWidgetTemplateStatusBarMixin, "Setup", function(widgetInfo)
+		local bar = widgetInfo.Bar
+		local atlas = bar:GetStatusBarAtlas()
+		updateBarTexture(bar, atlas)
+
+		if not bar.styled then
+			bar.BGLeft:SetAlpha(0)
+			bar.BGRight:SetAlpha(0)
+			bar.BGCenter:SetAlpha(0)
+			bar.BorderLeft:SetAlpha(0)
+			bar.BorderRight:SetAlpha(0)
+			bar.BorderCenter:SetAlpha(0)
+			bar.Spark:SetAlpha(0)
+			F.SetBD(bar)
+
+			bar.styled = true
+		end
+	end)
+
+	hooksecurefunc(_G.UIWidgetTemplateScenarioHeaderCurrenciesAndBackgroundMixin, "Setup", function(widgetInfo)
+		widgetInfo.Frame:SetAlpha(0)
+	end)
 end)
