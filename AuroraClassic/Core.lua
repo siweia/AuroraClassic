87f715b28069b84572cfbbd53824d93241581342
siweia <siwei1990@outlook.com>
2020-06-28 01:40:21 +0800

    Combine set alpha

--- AuroraClassic/Core.lua
+++ AuroraClassic/Core.lua
@@ -685,6 +685,7 @@ local function resetIconBorderColor(self)
 	self.__owner.bg:SetBackdropBorderColor(0, 0, 0)
 end
 function F:HookIconBorderColor()
+	self:SetAlpha(0)
 	self.__owner = self:GetParent()
 	if not self.__owner.bg then return end
 	hooksecurefunc(self, "SetVertexColor", updateIconBorderColor)
