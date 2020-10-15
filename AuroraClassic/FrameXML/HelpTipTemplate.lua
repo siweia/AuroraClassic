ba99f07092f92f1a3c4f5d253e198ffd291055a2
siweia <siwei1990@outlook.com>
2020-10-15 22:49:52 +0800

    Update skins for HelpTip

--- AuroraClassic/FrameXML/HelpTipTemplate.lua
+++ AuroraClassic/FrameXML/HelpTipTemplate.lua
@@ -1,20 +1,18 @@
 local _, ns = ...
 local F, C = unpack(ns)
 
-tinsert(C.defaultThemes, function()
-
-	local function reskinAlertFrame(frame)
+local function reskinHelpTips(self)
+	for frame in self.framePool:EnumerateActive() do
 		if not frame.styled then
 			if frame.OkayButton then F.Reskin(frame.OkayButton) end
 			if frame.CloseButton then F.ReskinClose(frame.CloseButton) end
-
+	
 			frame.styled = true
 		end
 	end
+end
 
-	hooksecurefunc(HelpTip, "Show", function(self)
-		for frame in self.framePool:EnumerateActive() do
-			reskinAlertFrame(frame)
-		end
-	end)
+tinsert(C.defaultThemes, function()
+	reskinHelpTips(HelpTip)
+	hooksecurefunc(HelpTip, "Show", reskinHelpTips)
 end)
