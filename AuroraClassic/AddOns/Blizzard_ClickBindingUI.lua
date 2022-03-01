6a7ba6af4ccdbb6341d1dcf21e4765d0515852f3
siweia <siwei1990@outlook.com>
2022-03-01 21:29:49 +0800

    Reskin tutorial in click binding

--- AuroraClassic/AddOns/Blizzard_ClickBindingUI.lua
+++ AuroraClassic/AddOns/Blizzard_ClickBindingUI.lua
@@ -78,4 +78,10 @@ C.themes["Blizzard_ClickBindingUI"] = function()
 
 	reskinPortraitIcon(frame.SpellbookPortrait, 136830)
 	reskinPortraitIcon(frame.MacrosPortrait, 136377)
+
+	frame.TutorialFrame.Bg:Hide()
+	frame.TutorialFrame.NineSlice:Hide()
+	frame.TutorialFrame.TitleBg:Hide()
+	F.SetBD(frame.TutorialFrame)
+	F.ReskinClose(frame.TutorialFrame.CloseButton)
 end
