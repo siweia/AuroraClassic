8819bdf138fbdf9d060597b4d33d8fc7120b04f5
siweia <siwei1990@outlook.com>
2022-03-01 21:30:11 +0800

    Remove rank background in orderhall UI

--- AuroraClassic/AddOns/Blizzard_GarrisonUI.lua
+++ AuroraClassic/AddOns/Blizzard_GarrisonUI.lua
@@ -1355,5 +1355,14 @@ C.themes["Blizzard_OrderHallUI"] = function()
 				end
 			end
 		end
+
+		if self.talentRankPool then
+			for rank in self.talentRankPool:EnumerateActive() do
+				if not rank.styled then
+					rank.Background:SetAlpha(0)
+					rank.styled = true
+				end
+			end
+		end
 	end)
 end
