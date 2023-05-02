a50d8e6e025a64efa01ee3994dab09c187d5012a
siweia <siwei1990@outlook.com>
2023-05-02 16:49:17 +0800

    More updates for 10.1

--- AuroraClassic/FrameXML/QuestMapFrame.lua
+++ AuroraClassic/FrameXML/QuestMapFrame.lua
@@ -64,6 +64,7 @@ tinsert(C.defaultThemes, function()
 	ReskinQuestHeader(campaignOverview.Header)
 
 	if DB.isPatch10_1 then
+		QuestScrollFrame.Edge:Hide()
 		B.ReskinTrimScroll(QuestScrollFrame.ScrollBar)
 		B.ReskinTrimScroll(campaignOverview.ScrollFrame.ScrollBar)
 	else
